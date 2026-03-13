3a99e0b3f0e4640000082c1116b8ee093d9d2c6f
Sitthikorn <phraibueng12@gmail.com>
2026-03-14 01:41:58 +0700

    x

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -38,12 +38,12 @@ const Qualification = () => {
                     ? "qualification__content qualification__content-active"  : "qualification__content"}>
                     <div className="qualification__data">
                         <div>
-                        <h3 className="qualification__title">Web Design</h3>
+                        <h3 className="qualification__title">Electronics Computer Technology</h3>
                             <span 
-                            className="qualification__subtitle">Spain - Institute</span>
+                            className="qualification__subtitle">King Mongkut's North Bangkok</span>
                             <div className="qualification__calendar">
                                 <i className="uil uil-calendar-alt"></
-                                i> 2021-Present
+                                i> 2023-Present
                             </div>
                         </div>
 
@@ -63,24 +63,24 @@ const Qualification = () => {
                         </div>
                         
                         <div>
-                        <h3 className="qualification__title">Art Director</h3>
+                        <h3 className="qualification__title">Diploma in Computer Technology</h3>
                             <span 
-                            className="qualification__subtitle">Spain - Institute</span>
+                            className="qualification__subtitle">Sisaket Technical colleague</span>
                             <div className="qualification__calendar">
                                 <i className="uil uil-calendar-alt"></
-                                i> 2020 - 2021
+                                i> 2021 - 2023
                             </div>
                         </div>
                     </div>
 
                     <div className="qualification__data">
                         <div>
-                        <h3 className="qualification__title">Web Development</h3>
+                        <h3 className="qualification__title">Vocational certificate</h3>
                             <span 
-                            className="qualification__subtitle">Spain - Institute</span>
+                            className="qualification__subtitle">Sisaket Technical Colleague</span>
                             <div className="qualification__calendar">
                                 <i className="uil uil-calendar-alt"></
-                                i> 2018-2020
+                                i> 2018-2021
                             </div>
                         </div>
 
@@ -88,33 +88,16 @@ const Qualification = () => {
                             <span className="qualification__rounder"></
                             span>
                             <span className="qualification__line"></span>
-                        </div>
-                    </div>
-
-                    <div className="qualification__data">
-                        <div></div>
-
-                        <div>
-                        <span className="qualification__rounder"></span>
-                        <span className="qualification__line"></span>
-                        </div>
-                        
-                        <div>
-                        <h3 className="qualification__title">UX Expert</h3>
-                            <span 
-                            className="qualification__subtitle">Spain - Institute</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></
-                                i> 2017-2018
-                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                <div className={
+                    toggleState === 2 
+                    ? "qualification__content qualification__content-active" : "qualification__content"}>
                     <div className="qualification__data">
                         <div>
-                        <h3 className="qualification__title">Product Designer</h3>
+                        <h3 className="qualification__title">Internship</h3>
                             <span 
                             className="qualification__subtitle">Microsoft - Spain</span>
                             <div className="qualification__calendar">
@@ -139,7 +122,7 @@ const Qualification = () => {
                         </div>
                         
                         <div>
-                        <h3 className="qualification__title">UX Designer</h3>
+                        <h3 className="qualification__title">Internship</h3>
                             <span 
                             className="qualification__subtitle">Apple Inc - Spain</span>
                             <div className="qualification__calendar">
@@ -149,23 +132,6 @@ const Qualification = () => {
                         </div>
                     </div>
 
-                    <div className="qualification__data">
-                        <div>
-                        <h3 className="qualification__title">Web Designer</h3>
-                            <span 
-                            className="qualification__subtitle">Figma - Spain</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></
-                                i> 2018-2020
-                            </div>
-                        </div>
-
-                        <div>
-                            <span className="qualification__rounder"></
-                            span>
-                            <span className="qualification__line"></span>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
